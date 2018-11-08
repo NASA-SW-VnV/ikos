c1225bcc9bcfbced4cf43872f140a99aa82812ea
Maxime Arthaud <maxime.arthaud@nasa.gov>
2018-11-08 14:06:01 -0800

    [analyzer][scan] Define LD as 'ikos-scan-cc' in ikos-scan

--- analyzer/python/ikos/scan.py
+++ analyzer/python/ikos/scan.py
@@ -792,6 +792,7 @@ def main(argv):
     os.environ['IKOS_SCAN_SERVER'] = 'http://localhost:%d' % server.port
     os.environ['CC'] = settings.ikos_scan_cc()
     os.environ['CXX'] = settings.ikos_scan_cxx()
+    os.environ['LD'] = settings.ikos_scan_cc()
 
     # run the build command
     rc = run(opt.args)
