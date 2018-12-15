36a4834c2a29220a81e6a04ad1bb99fe2d4eddf4
Maxime Arthaud <maxime.arthaud@nasa.gov>
2018-12-15 09:13:47 -0800

    [analyzer][scan] Use file names instead of full paths for CC, CXX and LD

--- analyzer/python/ikos/scan.py
+++ analyzer/python/ikos/scan.py
@@ -805,9 +805,10 @@ def main(argv):
     os.environ['IKOS_SCAN_COLOR'] = 'yes' if colors.ENABLE else 'no'
     os.environ['IKOS_SCAN_LOG_LEVEL'] = opt.log_level
     os.environ['IKOS_SCAN_SERVER'] = 'http://localhost:%d' % server.port
-    os.environ['CC'] = settings.ikos_scan_cc()
-    os.environ['CXX'] = settings.ikos_scan_cxx()
-    os.environ['LD'] = settings.ikos_scan_cc()
+    os.environ['PATH'] = settings.BIN_DIR + os.path.pathsep + os.environ['PATH']
+    os.environ['CC'] = 'ikos-scan-cc'
+    os.environ['CXX'] = 'ikos-scan-c++'
+    os.environ['LD'] = 'ikos-scan-cc'
 
     # add -e to make commands, to avoid makefiles overriding CC/CXX/LD
     if os.path.basename(opt.args[0]) == 'make':
