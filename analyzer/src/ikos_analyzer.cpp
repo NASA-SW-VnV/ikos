6bbebc9abce905b079a0426e3ab9aa74f1fbc831
Maxime Arthaud <maxime.arthaud@nasa.gov>
2019-08-29 16:24:09 -0700

    [analyzer] Use `dynamic_cast` to downcast from a base to a derived class
    This is reported by clang-tidy, check
    'cppcoreguidelines-pro-type-static-cast-downcast'

--- analyzer/src/ikos_analyzer.cpp
+++ analyzer/src/ikos_analyzer.cpp
@@ -650,7 +650,8 @@ static bool colors_enabled() {
   auto it = opts.find("color");
   ikos_assert_msg(it != opts.end(), "Option 'color' is not registered");
   auto opt =
-      static_cast< llvm::cl::opt< llvm::cl::boolOrDefault >* >(it->second);
+      dynamic_cast< llvm::cl::opt< llvm::cl::boolOrDefault >* >(it->second);
+  ikos_assert(opt != nullptr);
   if (opt->getValue() == llvm::cl::BOU_UNSET) {
     return llvm::sys::Process::StandardOutIsDisplayed();
   } else {
